1f48a83d43db91a3cfd3080dbdca25cb6c0a2b06
Felipe Acosta <fibacosta@gmail.com>
2024-02-21 22:02:15 -0300

    avances inicio sesión

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export default class LoginComponent {
+
+  public loginForm: FormGroup;
+  
+
+  constructor( private formBuilder: FormBuilder){
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  public iniciarSesion(): void{
+    console.log(this.loginForm);
+  }
 
 }
